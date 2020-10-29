8a5c43e8ab32034c572d79ffba57691480566277
Gabriel Rodrigues <jipacoding@gmail.com>
2020-10-29 07:12:31 -0400

    feat(node): [post] api/v1/users/login login user

---
+++ node/app/controllers/UsersController/login/index.ts
@@ -0,0 +1,31 @@
+import { RequestHandler } from "express";
+import * as yup from "yup";
+import User from "../../../models/User";
+
+export const UsersLoginValidation = yup.object().shape({
+  account: yup
+    .object()
+    .shape({
+      username: yup
+        .string()
+        .trim()
+        .matches(/^[a-zA-Z0-9]+$/)
+        .required(),
+      password: yup.string().min(5).matches(/\d/).required(),
+    })
+    .required(),
+});
+
+export const UsersLogin: RequestHandler = async (req, res) => {
+  const { username, password } = req.body.account;
+  return await User.findOne({ username }).then((user) => {
+    if (user && user.validPassword(password)) {
+      return res.json({ account: user.toAuthJSON() });
+    } else {
+      return res.status(422).json({
+        messages: ["username or password does not match"],
+        errors: [],
+      });
+    }
+  });
+};
--- node/app/routes/api/v1/users/index.ts
+++ node/app/routes/api/v1/users/index.ts
@@ -1,5 +1,9 @@
 import express from "express";
 import {
+  UsersLogin,
+  UsersLoginValidation,
+} from "../../../../controllers/UsersController/login";
+import {
   UsersNew,
   UsersNewValidation,
 } from "../../../../controllers/UsersController/new";
@@ -7,5 +11,6 @@ import BodyValidatorMiddleware from "../../../../middlewares/body-validator-midd
 
 const users = express.Router();
 users.post("/", BodyValidatorMiddleware(UsersNewValidation), UsersNew);
+users.post("/login", BodyValidatorMiddleware(UsersLoginValidation), UsersLogin);
 
 export default users;
