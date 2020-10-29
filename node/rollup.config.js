aff0bde0ef727366298d10f11d6115b1e7ce4897
Gabriel Rodrigues <jipacoding@gmail.com>
2020-10-29 10:44:34 -0400

    feat(node): generate bundle with rollup (see #1)

---
+++ node/rollup.config.js
@@ -0,0 +1,22 @@
+import commonjs from "@rollup/plugin-commonjs";
+import json from "@rollup/plugin-json";
+import resolve from "@rollup/plugin-node-resolve";
+import typescript from "@rollup/plugin-typescript";
+import { dependencies } from "./package.json";
+
+export default [
+  {
+    input: "app/app.ts",
+    output: {
+      file: "build/app.js",
+      format: "cjs",
+    },
+    plugins: [
+      json(),
+      resolve({ preferBuiltins: true }),
+      commonjs(),
+      typescript(),
+    ],
+    external: [...Object.keys({ ...dependencies })],
+  },
+];
