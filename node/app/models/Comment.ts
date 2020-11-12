c6f9cfd93b48bf654ef4609d28c93427cf78a6f4
Gabriel Rodrigues <jipacoding@gmail.com>
2020-11-12 14:39:32 -0400

    feat(node): Comment

---
+++ node/app/models/Comment.ts
@@ -0,0 +1,45 @@
+import { ICommentDoc } from "@/interfaces/IModelComment";
+import { User } from "@/models/User";
+import { Visibility } from "@/models/Visibility";
+import * as mongoose from "mongoose";
+
+const CommentSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, required: true, ref: User },
+  visibility: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: Visibility,
+  },
+  revisions: {
+    default: () => [],
+    required: true,
+    type: [
+      {
+        richTextContent: {
+          type: mongoose.Schema.Types.String,
+          required: true,
+          minlength: 1,
+        },
+        revTime: {
+          type: mongoose.Schema.Types.Date,
+          required: true,
+          default: Date.now,
+        },
+      },
+    ],
+  },
+  createTime: {
+    type: mongoose.Schema.Types.Date,
+    required: true,
+    default: Date.now,
+  },
+  repplyTo: { type: mongoose.Schema.Types.ObjectId },
+  replies: { type: [{ type: mongoose.Schema.Types.ObjectId }] },
+});
+
+export const Comment = mongoose.model<ICommentDoc>("Comment", CommentSchema);
+
+CommentSchema.add({
+  repplyTo: { type: mongoose.Schema.Types.ObjectId, ref: Comment },
+  replies: { type: [{ type: mongoose.Schema.Types.ObjectId, ref: Comment }] },
+});
