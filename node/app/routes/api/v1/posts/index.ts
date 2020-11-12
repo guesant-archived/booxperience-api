ed5c53c12a7f9b03bfe6b7288a4d67c0c5517011
Gabriel Rodrigues <jipacoding@gmail.com>
2020-11-12 14:42:47 -0400

    feat(node): posts: get-post

--- node/app/routes/api/v1/posts/index.ts
+++ node/app/routes/api/v1/posts/index.ts
@@ -1,11 +1,14 @@
 import * as addPostRevision from "@/controllers/PostController/add-post-revision";
 import * as deletePost from "@/controllers/PostController/delete-post";
+import * as getPost from "@/controllers/PostController/get-post";
 import * as newPost from "@/controllers/PostController/new-post";
 import { AuthRequired } from "@/middlewares/auth";
 import BodyValidatorMiddleware from "@/middlewares/body-validator-middleware";
 import express, { RequestHandler } from "express";
 
 const posts = express.Router();
+
+posts.get("/id/:id", AuthRequired, [getPost.GetPost] as RequestHandler[]);
 
 posts.post(
   "/",
