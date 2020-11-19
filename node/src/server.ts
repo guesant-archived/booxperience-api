7dc46117726a2be8de07a15af5950f85224cd417
Gabriel Rodrigues <jipacoding@gmail.com>
2020-11-19 08:06:33 -0400

    feat(node): [server.ts]: start expressApp

--- node/src/server.ts
+++ node/src/server.ts
@@ -1,6 +1,13 @@
+import { ExpressApp } from "@/ExpressApp";
+import { Router } from "@/routing/Router";
+import { Security } from "@/security/Security";
 import { setupDB } from "@/utils/setup-db";
 import config from "config";
 
 export async function startServer() {
   await setupDB({ dbUri: config.get("api.db.dbMongoURI") as string });
+  const router = new Router([]);
+  const security = new Security(config.get("api.security.jwtSecret"));
+  const expressApp = new ExpressApp(router, security);
+  expressApp.run();
 }
