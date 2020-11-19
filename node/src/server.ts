df1bb90a224f8484fa18ac1b96b5f302f924d18b
Gabriel Rodrigues <jipacoding@gmail.com>
2020-11-19 11:01:08 -0400

    feat(node): [controller] UsersController

--- node/src/server.ts
+++ node/src/server.ts
@@ -1,6 +1,7 @@
 import { ExpressApp } from "@/ExpressApp";
 import { Router } from "@/routing/Router";
 import { UserRoutes } from "@/routing/routes/api/v1/UserRoutes";
+import { UsersRoutes } from "@/routing/routes/api/v1/UsersRoutes";
 import { IndexRoutes } from "@/routing/routes/api/v1/IndexRoutes";
 import { Security } from "@/security/Security";
 import { setupDB } from "@/utils/setup-db";
@@ -11,6 +12,7 @@ export async function startServer() {
   const router = new Router([
     new IndexRoutes(),
     new UserRoutes(),
+    new UsersRoutes(),
   ]);
   const security = new Security(config.get("api.security.jwtSecret"));
   const expressApp = new ExpressApp(router, security);
