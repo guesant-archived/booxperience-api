fb7b224f302a2ea816884a5a874aaf7ac10cea6b
Gabriel Rodrigues <jipacoding@gmail.com>
2020-11-19 07:32:56 -0400

    feat(node): [app.ts]: run startServer

--- node/src/app.ts
+++ node/src/app.ts
@@ -1,37 +1,7 @@
-import { ResponseError } from "@/interfaces/ResponseError";
-import routes from "@/routes";
-import { setupDB } from "@/utils/setup-db";
-import config from "config";
-import cors from "cors";
-import errorhandler from "errorhandler";
-import express from "express";
-import helmet from "helmet";
-import morgan from "morgan";
-
-const isProduction = !config.get("debug");
+import { startServer } from "@/server";
 
 async function main() {
-  const app = express();
-  app.use(helmet());
-  config.has("api.logs") &&
-    config.get("api.logs") &&
-    app.use(morgan(config.get("api.logs")));
-  app.use(cors());
-  app.use(express.json());
-  app.use(express.static(__dirname + "/public"));
-  if (!isProduction) {
-    app.use(errorhandler());
-  }
-  await setupDB();
-  app.use("/", routes);
-  app.use((_req, _res, next) => {
-    next(new ResponseError("Not Found", 404));
-  });
-  const host: string = config.get("api.host");
-  const port: number = config.get("api.port");
-  app.listen(port, host, () => {
-    console.log(`[info] - Listening at http://${host}:${port}`);
-  });
+  await startServer();
 }
 
 main();
