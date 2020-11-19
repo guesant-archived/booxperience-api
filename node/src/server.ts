0657daa8a974b9e33ccff1db6a90474149f6c32f
Gabriel Rodrigues <jipacoding@gmail.com>
2020-11-19 08:04:24 -0400

    feat(node): [server.ts]: run setupDB

--- node/src/server.ts
+++ node/src/server.ts
@@ -1,2 +1,6 @@
+import { setupDB } from "@/utils/setup-db";
+import config from "config";
+
 export async function startServer() {
+  await setupDB({ dbUri: config.get("api.db.dbMongoURI") as string });
 }
--- node/src/utils/setup-db.ts
+++ node/src/utils/setup-db.ts
@@ -1,10 +1,10 @@
 import config from "config";
 import mongoose from "mongoose";
 
-export const setupDB = async () => {
+export const setupDB = async ({ dbUri }: { dbUri: string }) => {
   config.get("debug") && mongoose.set("debug", true);
   mongoose.set("useNewUrlParser", true);
   mongoose.set("useUnifiedTopology", true);
-  await mongoose.connect(config.get("api.db.dbMongoURL") as string);
+  await mongoose.connect(dbUri);
   return mongoose;
 };
